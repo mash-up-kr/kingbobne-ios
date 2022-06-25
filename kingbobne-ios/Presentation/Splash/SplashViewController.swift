d13ee1e68580e039a2569cf99ef9a6d279d2d24a
victhor <victhor@hpcnt.com>
2022-06-25 17:56:43 +0900

    add folder architecture

---
+++ kingbobne-ios/Presentation/Splash/SplashViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SplashViewController.swift
+//  kingbobne-ios
+//
+//  Created by 강대규 on 2022/06/25.
+//
+
+import Foundation
---
+++ kingbobne-ios/Repository/SignRepository/SignRepository.swift
@@ -0,0 +1,8 @@
+//
+//  SignRepository.swift
+//  kingbobne-ios
+//
+//  Created by 강대규 on 2022/06/25.
+//
+
+import Foundation
