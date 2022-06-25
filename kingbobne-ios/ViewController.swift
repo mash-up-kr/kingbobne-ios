7ef3e79ed62be32e8676862fe6814974cbc12d65
victhor <victhor@hpcnt.com>
2022-06-25 16:45:59 +0900

    Initial Commit

---
+++ kingbobne-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kingbobne-ios
+//
+//  Created by 강대규 on 2022/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
