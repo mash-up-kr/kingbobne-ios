2eb12d36cd1ade012e4a62586b0a8f868efa0b3d
parksohyeon <sohypark@smilegate.com>
2022-07-17 15:27:41 +0900

    RootVC title 추가

--- kingbobne-ios/Sources/Presentation/MainTabBarController.swift
+++ kingbobne-ios/Sources/Presentation/MainTabBarController.swift
@@ -19,10 +19,6 @@ class MainTabBarController: UITabBarController {
         let postKkiLogViewController = UIStoryboard(name: "Kkilog", bundle: nil).instantiateViewController(withIdentifier: "PostKkiLogViewController") as! PostKkiLogViewController
         let myCookingViewController = UIStoryboard(name: "MyCooking", bundle: nil).instantiateViewController(withIdentifier: "MyCookingViewController") as! MyCookingViewController
         
-//        homeViewController.title = "홈"
-//        postKkiLogViewController.title = "끼록하기"
-//        myCookingViewController.title = "내 요리"
-        
         [homeViewController, postKkiLogViewController, myCookingViewController].forEach({
             $0?.navigationItem.largeTitleDisplayMode = .always
         })
