7c3a4247e4e829a47c364e7dee2f01c4eb6ddc3e
parksohyeon <sohypark@smilegate.com>
2022-07-17 16:08:30 +0900

    MainTabBar 로직 추가

--- kingbobne-ios/Sources/Presentation/MainTabBarController.swift
+++ kingbobne-ios/Sources/Presentation/MainTabBarController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
 
+    var homeNavigationController = UINavigationController()
+    var postKkiLogNavigationController = UINavigationController()
+    var myCookingNavigationController = UINavigationController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tabBar.tintColor = .Custom.brandOrange
+        delegate = self
         
         let homeViewController = UIStoryboard(name: "Home", bundle: nil).instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
         let postKkiLogViewController = UIStoryboard(name: "Kkilog", bundle: nil).instantiateViewController(withIdentifier: "PostKkiLogViewController") as! PostKkiLogViewController
@@ -23,17 +28,59 @@ class MainTabBarController: UITabBarController {
             $0?.navigationItem.largeTitleDisplayMode = .always
         })
         
-        let homeNavigationController = UINavigationController(rootViewController: homeViewController)
-        let postKkiLogNavigationController = UINavigationController(rootViewController: postKkiLogViewController)
-        let myCookingNavigationController = UINavigationController(rootViewController: myCookingViewController)
+        homeNavigationController = UINavigationController(rootViewController: homeViewController)
+        postKkiLogNavigationController = UINavigationController(rootViewController: postKkiLogViewController)
+        myCookingNavigationController = UINavigationController(rootViewController: myCookingViewController)
         
-        homeNavigationController.tabBarItem.image = UIImage.ic_home_disable_44.withRenderingMode(.alwaysOriginal)
         postKkiLogNavigationController.tabBarItem.image = UIImage.ic_add_44.withRenderingMode(.alwaysOriginal)
-        myCookingNavigationController.tabBarItem.image = UIImage.ic_recipe_disable_44.withRenderingMode(.alwaysOriginal)
         
+        selectHomeTab()
         setViewControllers([homeNavigationController, postKkiLogNavigationController, myCookingNavigationController], animated: false)
-        
-        
+    }
+    
+    func selectHomeTab() {
+        setHomeTabItem(isSelect: true)
+        setMyCookingTabItem(isSelect: false)
+    }
+    
+    func selectPostKkiLogTab() {
+        setHomeTabItem(isSelect: false)
+        setMyCookingTabItem(isSelect: false)
+    }
+    
+    func selectMyCookingTab() {
+        setHomeTabItem(isSelect: false)
+        setMyCookingTabItem(isSelect: true)
+    }
+    
+    private func setHomeTabItem(isSelect: Bool) {
+        if isSelect {
+            homeNavigationController.tabBarItem.image = UIImage.ic_home_active_44.withRenderingMode(.alwaysOriginal)
+        } else {
+            homeNavigationController.tabBarItem.image = UIImage.ic_home_disable_44.withRenderingMode(.alwaysOriginal)
+        }
+    }
+    
+    private func setMyCookingTabItem(isSelect: Bool) {
+        if isSelect {
+            myCookingNavigationController.tabBarItem.image = UIImage.ic_recipe_active_44.withRenderingMode(.alwaysOriginal)
+        } else {
+            myCookingNavigationController.tabBarItem.image = UIImage.ic_recipe_disable_44.withRenderingMode(.alwaysOriginal)
+        }
     }
 
+}
+extension MainTabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        switch viewController {
+        case homeNavigationController:
+            selectHomeTab()
+        case postKkiLogNavigationController:
+            selectPostKkiLogTab()
+        case myCookingNavigationController:
+            selectMyCookingTab()
+        default:
+            break
+        }
+    }
 }
