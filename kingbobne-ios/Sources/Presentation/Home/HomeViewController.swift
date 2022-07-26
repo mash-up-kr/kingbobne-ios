8207c4ba6d65a3702b3a1583d1d2ef7ac9d6af68
parksohyeon <sohypark@smilegate.com>
2022-07-26 23:02:14 +0900

    HomeViewController 하단 정보 뷰 UI

--- kingbobne-ios/Sources/Presentation/Home/HomeViewController.swift
+++ kingbobne-ios/Sources/Presentation/Home/HomeViewController.swift
@@ -7,15 +7,19 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+final class HomeViewController: UIViewController {
 
     @IBOutlet weak var friendCollectionView: UICollectionView!
+    @IBOutlet var bottomViewDividers: [UIView]!
+    @IBOutlet weak var recommendMenuButton: ClearButton!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         setCollectionView()
+        setUIComponent()
     }
     
     private func setCollectionView() {
@@ -23,6 +27,22 @@ class HomeViewController: UIViewController {
         friendCollectionView.dataSource = self
         friendCollectionView.showsHorizontalScrollIndicator = false
         friendCollectionView.register(UINib(nibName: "HomeFriendCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "HomeFriendCollectionViewCell")
+    }
+    
+    private func setUIComponent() {
+        bottomViewDividers.forEach({
+            $0.backgroundColor = .Custom.brownGray300
+        })
+        descriptionLabel.font = .setFont(style: .Body2Regular)
+        descriptionLabel.textColor = .Custom.brownGray200
+        questionLabel.font = .setFont(style: .Body1Regular)
+        questionLabel.textColor = .Custom.brownGray400
+        
+        recommendMenuButton.setButtonStyle(text: "뭐먹지", fontStyle: .Body2Regular, fontColor: .Custom.brownGray400)
+        recommendMenuButton.imageView?.sizeThatFits(CGSize(width: 32, height: 32))
+        
+        descriptionLabel.text = "일이삼사오육칠팝구십"
+        questionLabel.text = "해가 중천인데 배 안고파요?"
     }
 
 }
