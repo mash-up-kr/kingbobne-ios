c3a1963037b4f46c443d14c2974b9cb8aa3a92c8
Sohyeon Park <sohyun1018@gmail.com>
2022-08-07 16:30:54 +0900

    Feature/home (#9)
    * HomeViewController - 친구리스트 컬렉션 뷰 setup
    
    * HomeViewController 하단 정보 뷰 UI
    
    * HomeVC 에셋 추가
    
    * HomeViewController - 상단바 액션 추가
    
    * HomeViewController 캐릭터 이미지 뷰 추가
    
    * test commit
    
    * warning제거 : CollectionView Cell Identifier 추가
    
    * UIView Extension - loadXib 추가
    
    * HomeVC - HomeInfoView UI
    
    * HomeVC - FriendCollectionViewCell UI 수정
    
    * HomeVC - 끼록하기 버튼 addTarget 추가
    
    Co-authored-by: parksohyeon <sohypark@smilegate.com>

--- kingbobne-ios/Sources/Presentation/Home/HomeViewController.swift
+++ kingbobne-ios/Sources/Presentation/Home/HomeViewController.swift
@@ -11,9 +11,84 @@ class HomeViewController: UIViewController {
     
     private let viewModel: HomeViewModel = HomeViewModelCompanion.newInstance()
 
+    @IBOutlet weak var friendCollectionView: UICollectionView!
+    @IBOutlet var bottomViewDividers: [UIView]!
+    @IBOutlet weak var recommendMenuButton: ClearButton!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var myPageButton: UIButton!
+    @IBOutlet weak var alarmButton: UIButton!
+    @IBOutlet weak var characterImageView: UIImageView!
+    @IBOutlet weak var homeInfoView: HomeInfoView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setCollectionView()
+        setUIComponent()
+        homeInfoView.postKkilogButton.addTarget(self, action: #selector(addKkilogAction), for: .touchUpInside)
         
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.navigationBar.isHidden = false
+    }
+    
+    private func setCollectionView() {
+        friendCollectionView.delegate = self
+        friendCollectionView.dataSource = self
+        friendCollectionView.showsHorizontalScrollIndicator = false
+        friendCollectionView.register(UINib(nibName: "HomeFriendCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "HomeFriendCollectionViewCell")
+    }
+    
+    private func setUIComponent() {
+        bottomViewDividers.forEach({
+            $0.backgroundColor = .Custom.brownGray300
+        })
+        descriptionLabel.font = .setFont(style: .Body2Regular)
+        descriptionLabel.textColor = .Custom.brownGray200
+        questionLabel.font = .setFont(style: .Body1Regular)
+        questionLabel.textColor = .Custom.brownGray400
+        
+        recommendMenuButton.setButtonStyle(text: "뭐먹지", fontStyle: .Body2Regular, fontColor: .Custom.brownGray400)
+        recommendMenuButton.imageView?.sizeThatFits(CGSize(width: 32, height: 32))
+        
+        descriptionLabel.text = "일이삼사오육칠팝구십"
+        questionLabel.text = "해가 중천인데 배 안고파요?"
+        
+        myPageButton.setImage(UIImage.ic_mypage_44, for: .normal)
+        alarmButton.setImage(UIImage.ic_alarm_44, for: .normal)
+        [myPageButton, alarmButton].forEach({
+            $0?.tintColor = UIColor(red: 80/255, green: 80/255, blue: 80/255, alpha: 1.0)
+        })
+        characterImageView.image = UIImage(named: "Lv3_배부를때")
+    }
+    
+    @objc func addKkilogAction(){
+        // TODO
+        print("addKkilogAction")
+    }
+    
+    @IBAction func onAlarmAction(_ sender: Any) {
+        // TODO
+    }
+    
+    @IBAction func onMyPageAction(_ sender: Any) {
+        // TODO
+    }
+}
+extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeFriendCollectionViewCell", for: indexPath) as! HomeFriendCollectionViewCell
+        return cell
+    }
 }
