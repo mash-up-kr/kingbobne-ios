0f91e0b671df0e798e988c8af84998eca779cf64
Sohyeon Park <sohyun1018@gmail.com>
2022-08-20 17:35:17 +0900

    끼록하기 - 간단끼록 / 상세끼록 바텀 시트 UI 및 끼록하기 연결 (#18)

--- kingbobne-ios/Sources/Presentation/KkiLog/SelectKkiLogViewController.swift
+++ kingbobne-ios/Sources/Presentation/KkiLog/SelectKkiLogViewController.swift
@@ -9,18 +9,44 @@
 import UIKit
 import PanModal
 
-class SelectKkiLogViewController: UIViewController, PanModalPresentable {
+final class SelectKkiLogViewController: UIViewController, PanModalPresentable {
     var panScrollable: UIScrollView? {
             return nil
     }
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var simpleKkilogButton: ClearButton!
+    @IBOutlet weak var detailKkilogButton: ClearButton!
     
     var shortFormHeight: PanModalHeight {
-        return .contentHeight(300)
+        return .contentHeight(170)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
         panModalSetNeedsLayoutUpdate()
+        setUI()
+    }
+    
+    private func setUI() {
+        titleLabel.font = .setFont(style: .Body1Bold)
+        titleLabel.textColor = .Custom.brownGray500
+        titleLabel.text = "끼록 추가"
+        simpleKkilogButton.setButtonStyle(text: "간단 끼록", fontStyle: .Body1Regular, fontColor: .Custom.brownGray500)
+        detailKkilogButton.setButtonStyle(text: "상세 끼록", fontStyle: .Body1Regular, fontColor: .Custom.brownGray500)
+        [simpleKkilogButton, detailKkilogButton].forEach({
+            $0?.contentHorizontalAlignment = .left
+        })
+    }
+    @IBAction func onSimpleKkilog(_ sender: Any) {
+        guard let simpleKkilogViewController = UIStoryboard(name: "SimpleKkilog", bundle: nil).instantiateViewController(withIdentifier: "PostSimpleKkiLogViewController") as? PostSimpleKkiLogViewController else { return }
+        
+        simpleKkilogViewController.modalPresentationStyle = .overFullScreen
+        self.present(simpleKkilogViewController, animated: true)
+        
+    }
+    @IBAction func onDetailKkilog(_ sender: Any) {
+        guard let detailKkilogViewController = UIStoryboard(name: "DetailKkilog", bundle: nil).instantiateViewController(withIdentifier: "PostDetailKkiLogViewController") as? PostDetailKkiLogViewController else { return }
+        detailKkilogViewController.modalPresentationStyle = .overFullScreen
+        self.present(detailKkilogViewController, animated: true)
     }
 }
