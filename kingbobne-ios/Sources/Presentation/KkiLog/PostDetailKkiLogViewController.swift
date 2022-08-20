5a2222178576bf594211e26e33a64dcc9b9a96c7
Sohyeon Park <sohyun1018@gmail.com>
2022-08-20 17:30:54 +0900

    끼록하기 - 간단끼록 / 상세끼록 바텀 시트 UI 및 끼록하기 연결

---
+++ kingbobne-ios/Sources/Presentation/KkiLog/PostDetailKkiLogViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PostDetailKkiLogViewController.swift
+//  kingbobne-ios
+//
+//  Created by sohyeon on 2022/08/20.
+//  Copyright © 2022 3999WG8MCQ. All rights reserved.
+//
+
+import UIKit
+
+class PostDetailKkiLogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
