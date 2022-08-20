f84209a7f8535d1f026e16172f497dace007d1fd
Sohyeon Park <sohyun1018@gmail.com>
2022-08-20 22:07:32 +0900

    add ImagePicker

--- kingbobne-ios/Sources/Presentation/KkiLog/SelectKkiLogViewController.swift
+++ kingbobne-ios/Sources/Presentation/KkiLog/SelectKkiLogViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import PanModal
+import YPImagePicker
 
 final class SelectKkiLogViewController: UIViewController, PanModalPresentable {
     var panScrollable: UIScrollView? {
@@ -17,6 +18,8 @@ final class SelectKkiLogViewController: UIViewController, PanModalPresentable {
     @IBOutlet weak var simpleKkilogButton: ClearButton!
     @IBOutlet weak var detailKkilogButton: ClearButton!
     
+    var picker: YPImagePicker?
+    
     var shortFormHeight: PanModalHeight {
         return .contentHeight(170)
     }
@@ -24,7 +27,20 @@ final class SelectKkiLogViewController: UIViewController, PanModalPresentable {
     override func viewDidLoad() {
         super.viewDidLoad()
         panModalSetNeedsLayoutUpdate()
+        configImagePicker()
         setUI()
+    }
+    
+    private func configImagePicker() {
+        var config = YPImagePickerConfiguration()
+        config.showsPhotoFilters = false
+        config.startOnScreen = .library
+        config.library.maxNumberOfItems = 10
+        config.library.mediaType = .photo
+        config.library.numberOfItemsInRow = 3
+        config.library.skipSelectionsGallery = true
+        
+        picker = YPImagePicker(configuration: config)
     }
     
     private func setUI() {
@@ -37,13 +53,33 @@ final class SelectKkiLogViewController: UIViewController, PanModalPresentable {
             $0?.contentHorizontalAlignment = .left
         })
     }
-    @IBAction func onSimpleKkilog(_ sender: Any) {
+    
+    func routeToPostSimpleKkilog() {
         guard let simpleKkilogViewController = UIStoryboard(name: "SimpleKkilog", bundle: nil).instantiateViewController(withIdentifier: "PostSimpleKkiLogViewController") as? PostSimpleKkiLogViewController else { return }
-        
+
         simpleKkilogViewController.modalPresentationStyle = .overFullScreen
         self.present(simpleKkilogViewController, animated: true)
-        
     }
+    
+    @IBAction func onSimpleKkilog(_ sender: Any) {
+        if let picker = picker {
+            picker.didFinishPicking { [unowned picker] items, _ in
+                for item in items {
+                    switch item {
+                    case .photo(let photo):
+                        print(photo.image)
+                    case .video(let video):
+                        print(video)
+                    }
+                }
+                picker.dismiss(animated: false, completion: {
+                    self.routeToPostSimpleKkilog()
+                })
+            }
+            present(picker, animated: true, completion: nil)
+        }
+    }
+    
     @IBAction func onDetailKkilog(_ sender: Any) {
         guard let detailKkilogViewController = UIStoryboard(name: "DetailKkilog", bundle: nil).instantiateViewController(withIdentifier: "PostDetailKkiLogViewController") as? PostDetailKkiLogViewController else { return }
         detailKkilogViewController.modalPresentationStyle = .overFullScreen
